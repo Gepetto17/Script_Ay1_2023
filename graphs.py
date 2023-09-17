639b27bfd00daa1f10d7f8661c9e47c6c83733ca
Gepetto17 <113725717+Gepetto17@users.noreply.github.com>
2023-09-17 20:47:25 -0300

    Create graphs.py
    Script para graficar el oscilador armónico. Preparado para clase/prueba de oposición de ayudante de primera 2023.

---
+++ graphs.py
@@ -0,0 +1,24 @@
+# Librerías
+import numpy as np
+import matplotlib.pyplot as plt
+# Funciones
+def sobre_amortiguado(y0,y0p,beta,delta,t):
+	return(((np.exp(-beta*t))*0.5)*((y0 + ((y0p + beta*y0)/delta))*np.exp(delta*t) + (y0 - ((y0p + beta*y0)/delta))*np.exp(-delta*t)))
+def sub_amortiguado(y0,y0p,beta,delta,t):
+	return((np.exp(-beta*t))*(y0*np.cos(delta*t) + ((y0p + beta*y0)/delta)*np.sin(delta*t)))
+def critico(y0,y0p,beta,delta,t):
+	return((np.exp(-beta*t))*(y0 + (y0p + beta*y0)*t))
+# Variables
+y0 = 100
+y0p = 0
+# Gráficos
+t = np.linspace(0,10,1000)
+fig, axs = plt.subplots(ncols=1, nrows=1, figsize=(10, 5))
+axs.plot(t,sobre_amortiguado(y0,y0p,1,0.5,t), label='Oscilador sobreamortiguado')
+axs.plot(t,sub_amortiguado(y0,y0p,0.3,1,t), label='Oscilador subamortiguado')
+axs.plot(t,critico(y0,y0p,1,0.1,t), label='Oscilador críticamente amortiguado')
+axs.set_ylabel('Amplitud')
+axs.set_xlabel('Tiempo')
+axs.legend()
+fig.tight_layout()
+plt.show()
